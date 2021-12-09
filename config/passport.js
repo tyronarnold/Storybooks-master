d7c2fb13a8f41ccf3de2bb94562124e92b2f39f1
Ty Arnold <tyarnold1@gmail.com>
2021-12-08 21:48:33 -0600

    Updated oauth id info

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,15 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const mongoose = require('mongoose')
-const User = require('../models/User')
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const User = require("../models/User");
 
 module.exports = function (passport) {
   passport.use(
     new GoogleStrategy(
       {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        clientID:
+          "317386116551-kaph0lfnihqaekqnc6q6er6if329dcer.apps.googleusercontent.com",
+        clientSecret: "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",
+        callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
@@ -17,29 +18,29 @@ module.exports = function (passport) {
           firstName: profile.name.givenName,
           lastName: profile.name.familyName,
           image: profile.photos[0].value,
-        }
+        };
 
         try {
-          let user = await User.findOne({ googleId: profile.id })
+          let user = await User.findOne({ googleId: profile.id });
 
           if (user) {
-            done(null, user)
+            done(null, user);
           } else {
-            user = await User.create(newUser)
-            done(null, user)
+            user = await User.create(newUser);
+            done(null, user);
           }
         } catch (err) {
-          console.error(err)
+          console.error(err);
         }
       }
     )
-  )
+  );
 
   passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
+    done(null, user.id);
+  });
 
   passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
-  })
-}
+    User.findById(id, (err, user) => done(err, user));
+  });
+};
